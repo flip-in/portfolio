cf56cda8f1baaedccc930cb55e5c8c7e26861936
Will Price <pricew86@gmail.com>
2023-12-17 21:06:04 +0800

    finish projects section

--- components/intro.tsx
+++ components/intro.tsx
@@ -53,7 +53,7 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
-      <motion.p
+      <motion.h1
         className='mb-10 mt-4, px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
         initial={{
           opacity: 0,
@@ -69,7 +69,7 @@ export default function Intro() {
         <span className='font-bold'>4 years</span> of experience. I enjoy
         building <span className='italic'>sites & apps</span>. My focus is{' '}
         <span className='underline'>React (Next.js)</span>.
-      </motion.p>
+      </motion.h1>
       <motion.div
         className='flex flex-col sm:flex-row justify-center items-center gap-2 px-4 text-lg font-medium'
         initial={{
