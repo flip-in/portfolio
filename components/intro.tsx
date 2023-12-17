0df08f54ac30e1d817fc0f9f48a47eb1a016d522
Will Price <pricew86@gmail.com>
2023-12-17 18:54:42 +0800

    initial commit plus header and intro

---
+++ components/intro.tsx
@@ -0,0 +1,120 @@
+'use client';
+
+import Image from 'next/image';
+import React from 'react';
+import profileImage from '../public/William - small.jpg';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
+import { HiDownload } from 'react-icons/hi';
+import { FaGithubSquare } from 'react-icons/fa';
+
+export default function Intro() {
+  return (
+    <section className='mb-28 max-w-[50rem] text-center sm:mb-0'>
+      <div className='flex items-center justify-center'>
+        <div className='relative'>
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: 'tween',
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src={profileImage}
+              alt='William portrait'
+              width={192}
+              height={192}
+              quality='95'
+              priority={true}
+              className='h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl'
+            />
+          </motion.div>
+          <motion.span
+            className='text-4xl absolute bottom-0 right-0'
+            initial={{
+              opacity: 0,
+              scale: 0,
+            }}
+            animate={{
+              opacity: 1,
+              scale: 1,
+            }}
+            transition={{
+              type: 'spring',
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+      <motion.p
+        className='mb-10 mt-4, px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
+        initial={{
+          opacity: 0,
+          y: 100,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+      >
+        <span className='font-bold'>Hello, I&apos;m William.</span> I&apos;m a{' '}
+        <span className='font-bold'>full-stack developer</span> with{' '}
+        <span className='font-bold'>4 years</span> of experience. I enjoy
+        building <span className='italic'>sites & apps</span>. My focus is{' '}
+        <span className='underline'>React (Next.js)</span>.
+      </motion.p>
+      <motion.div
+        className='flex flex-col sm:flex-row justify-center items-center gap-2 px-4 text-lg font-medium'
+        initial={{
+          opacity: 0,
+          y: 100,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href='#contact'
+          className='bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition group'
+        >
+          Contact me here{' '}
+          <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' />
+        </Link>
+
+        <a
+          href='/William Price Software Developer CV.pdf'
+          download
+          className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10 '
+        >
+          Download CV{' '}
+          <HiDownload className='group-hover:translate-y-1 transition opacity-70' />
+        </a>
+        <a
+          href='https://www.linkedin.com/in/price-william-alexander/'
+          target='_blank'
+          className='bg-white text-gray-700 hover:text-gray-950 p-4 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10 '
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href='https://github.com/flip-in'
+          target='_blank'
+          className='bg-white text-gray-700 hover:text-gray-950 p-4 flex items-center gap-2 rounded-full text-[1.35rem] focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10 '
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
+    </section>
+  );
+}
