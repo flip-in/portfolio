1b76c481c99a832dd44aec63edf93a2a55e2a7a4
Will Price <pricew86@gmail.com>
2023-12-18 22:57:06 +0800

    update contact email

--- components/contact.tsx
+++ components/contact.tsx
@@ -34,7 +34,7 @@ export default function Contact() {
       <SectionHeading>Contact me</SectionHeading>
       <p className='text-gray-700 -mt-5 dark:text-white/80 '>
         Please contact me directly at{' '}
-        <a className='underline' href='mailto:hello@william.price.dev'>
+        <a className='underline' href='mailto:hello@williamprice.dev'>
           hello@williamprice.dev{' '}
         </a>{' '}
         or through this form
