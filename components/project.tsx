6d11eeb8985d820b57bc7a59ba27cf609e529034
Will Price <pricew86@gmail.com>
2023-12-18 10:17:36 +0800

    skills section

--- components/project.tsx
+++ components/project.tsx
@@ -31,8 +31,8 @@ export default function Project({
       }}
       className='mb-3 sm:mb-8 last:mb-0 group'
     >
-      <section className='relative bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 sm:h-[20rem] group-even:pl-8 hover:bg-gray-200 transition rounded-lg'>
-        <div className='py-4 px-5 pb-7 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem] '>
+      <section className='relative bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 sm:h-[20rem] sm:group-even:pl-8 hover:bg-gray-200 transition rounded-lg'>
+        <div className='py-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem] '>
           <h3 className='text-2xl font-semibold '>{title}</h3>
           <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
           <ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
@@ -50,7 +50,16 @@ export default function Project({
           src={imageUrl}
           alt='Project I worked on'
           quality={95}
-          className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:right-[initial] group-even:-left-40 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.04] group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2'
+          className='absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl 
+          group-even:right-[initial] 
+          group-even:-left-40 
+          group-hover:-translate-x-3 
+          group-hover:translate-y-3 
+          group-hover:-rotate-2 
+          group-hover:scale-[1.04] 
+          group-even:group-hover:translate-x-3 
+          group-even:group-hover:translate-y-3 
+          group-even:group-hover:rotate-2'
         />
       </section>
     </motion.div>
