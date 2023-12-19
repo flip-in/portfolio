48a868a587091cdaca7b850e2b2afebf1e36e6a9
Will Price <pricew86@gmail.com>
2023-12-19 12:22:38 +0800

    update about me

--- components/about.tsx
+++ components/about.tsx
@@ -22,8 +22,9 @@ export default function About() {
       <SectionHeading>About me</SectionHeading>
       <p className='mb-3'>
         After graduating with a degree in{' '}
-        <span className='font-medium'>Philosophy</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{' '}
+        <span className='font-medium'>Philosophy and Computer Science</span>, I
+        decided to pursue my passion for programming. I enrolled in a coding
+        bootcamp and learned{' '}
         <span className='font-medium'>full-stack web development</span>.{' '}
         <span className='italic'>My favorite part of programming</span> is the
         problem-solving aspect. I <span className='underline'>love</span> the
