4270b0163c0a3eb33e2a7c8a298ae39fab09517b
Will Price <pricew86@gmail.com>
2023-12-19 12:20:59 +0800

    add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import Footer from '@/components/footer';
 import ThemeSwitch from '@/components/theme-switch';
 import ThemeContextProvider from '@/context/theme-context';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
             <Header />
             {children}
             <Analytics />
+            <SpeedInsights />
             <Toaster position='top-right' />
             <Footer />
             <ThemeSwitch />
