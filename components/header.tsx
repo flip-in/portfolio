609ea77243c2d456c352a4f8760392eba0be82a2
Will Price <pricew86@gmail.com>
2023-12-19 10:22:16 +0800

    attempt to fix header activeSection text color

--- components/header.tsx
+++ components/header.tsx
@@ -30,10 +30,10 @@ export default function Header() {
               <Link
                 href={link.hash}
                 className={clsx(
-                  'flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300',
+                  'flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:hover:text-gray-300',
                   {
-                    ' text-gray-950 dark:text-gray-200':
-                      activeSection === link.name,
+                    ' text-gray-950 ': activeSection === link.name,
+                    'dark:text-gray-200': activeSection === link.name,
                   }
                 )}
                 onClick={() => {
