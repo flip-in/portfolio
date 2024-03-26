41fe8f3ff78ee9365038f48a2cc5652b85c12ac2
Will Price <pricew86@gmail.com>
2024-03-26 16:50:46 +0100

    update about

--- components/about.tsx
+++ components/about.tsx
@@ -31,9 +31,9 @@ export default function About() {
         feeling of finally figuring out a solution to a problem. My core stack
         is{' '}
         <span className='font-medium'>
-          React, Next.js, Node.js, and MongoDB
+          React, TypeScript, Next.js, and MongoDB
         </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
+        . I am also familiar with Node.js and Prisma. I am always looking to
         learn new technologies. I am currently looking for a{' '}
         <span className='font-medium'>full-time position</span> as a software
         developer.
